8dd19c771185714414e8a951ad3bd81d4cb78817
yzy <3103623064@qq.com>
2026-05-29 18:58:04 +0800

    jiekou

--- HM_LICAI/a1.py
+++ HM_LICAI/a1.py
@@ -14,12 +14,12 @@ res2= seen.post(url ='http://121.43.169.97/member/public/sendSms',
                     "type":"reg"
                 },cookies=res.cookies)
 print(res2.json())
-res3= seen.post(url ='http://121.43.169.97/member/public/reg',
-         data={
-                    "phone":"18332005002",
-                    "password":"123456asd",
-                    "verifyCode":"888888",
-                   "phone_code":"666666",
-                    "dy_server":"on"
-                },cookies=res.cookies)
-print(res3.json())
+# res3= seen.post(url ='http://121.43.169.97/member/public/reg',
+#          data={
+#                     "phone":"18332005002",
+#                     "password":"123456asd",
+#                     "verifyCode":"8888",
+#                    "phone_code":"666666",
+#                     "dy_server":"on"
+#                 },cookies=res.cookies)
+# print(res3.json())
