c45fe8e826642d08ed4c261b194fe3f67687839d
yzy <3103623064@qq.com>
2026-05-30 14:40:02 +0800

    小更改

--- HM_test_pytest_jiekou/conftest.py
+++ HM_test_pytest_jiekou/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-@pytest.fixture(scope="module")  # 定义一个模块级别的fixture，确保在整个模块中只初始化和清理一次
+@pytest.fixture(scope="class")  # 定义一个模块级别的fixture，确保在整个模块中只初始化和清理一次
 def open_1():  # 定义名为open_1的fixture函数
     print("打开计算器")  # 在测试开始前执行，模拟打开计算器的操作
     yield  # 暂停执行，将控制权交给测试用例，测试结束后继续执行yield之后的代码
@@ -14,4 +14,10 @@ conftest.py：是 pytest 的一个特殊配置文件，用于定义全局或局
 fixture 定义在 conftest.py 中。
 ⚫ 全局配置：可以在 conftest.py 中定义一些全局的配置，如日志设置、
 环境变量等。测试方法中引用fixture装饰器定义函数名作为形参传入
+作用域
+function（默认值）：每个测试函数执行一次 fixture。
+⚫ class：每个测试类执行一次 fixture。
+⚫ module：每个测试模块执行一次 fixture。
+⚫ session：整个测试会话执行一次 fixture。
+
 """
